eda0388f789566e8a9008230893dabb48447ad6e
Thiago Vinícius <80261904+ubyss@users.noreply.github.com>
2022-09-13 20:44:06 -0300

    Quiz app

--- app/src/main/java/com/example/admob/MainActivity.kt
+++ app/src/main/java/com/example/admob/MainActivity.kt
@@ -1,24 +1,47 @@
 package com.example.admob
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        MobileAds.initialize(this) {}
+        val btnStart: Button = findViewById(R.id.startBtn)
+        val nameIpt: EditText = findViewById(R.id.nameIpt)
+        btnStart.setOnClickListener {
+            if (nameIpt.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, nameIpt.text.toString())
+                startActivity(intent)
+                finish()
+            }
+        }
 
-        mAdView = findViewById(R.id.adView)
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
     }
-
 }
+
+
+
+//    lateinit var mAdView : AdView
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        MobileAds.initialize(this) {}
+//
+//        mAdView = findViewById(R.id.adView)
+//        val adRequest = AdRequest.Builder().build()
+//        mAdView.loadAd(adRequest)
+//    }
+
