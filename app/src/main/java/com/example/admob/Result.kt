112b87d5a2e886e9ce31a54523790b796e0f013e
Monalysa <monalysaklauck@gmail.com>
2022-09-19 22:48:32 -0300

    subindo-banco-de-dados

--- app/src/main/java/com/example/admob/Result.kt
+++ app/src/main/java/com/example/admob/Result.kt
@@ -1,5 +1,6 @@
 package com.example.admob
 
+import android.app.Instrumentation.ActivityResult
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,11 +10,14 @@ import android.widget.Button
 import android.widget.ListView
 import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_result.*
 
 class Result : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +42,8 @@ class Result : AppCompatActivity() {
         btn_placar.setOnClickListener {
             startActivity(Intent(this, PlacarActivity::class.java))
         }
+
+
     }
 
     private fun getData(){
